59fb4de364ea00b09917b551e414bbe7f244212c
w4je <w4je@users.noreply.github.com>
2022-01-30 13:55:15 -0500

    Stub for top-level test function

---
+++ src/harness/test_main.py
@@ -0,0 +1,20 @@
+#    Copyright 2022 6 GHz AFC Project Authors. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+"""Top main script of the test harness.
+
+If writing your own top level script to run the test harness,
+look here how the setup is performed.
+It is particularly important to correctly set the geo drivers
+and multiprocessing pool for best performance
+"""
