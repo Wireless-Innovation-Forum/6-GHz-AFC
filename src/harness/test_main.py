187bda1676f9e4add04e0a4de9bcfcc4c15bbe04
w4je <w4je@users.noreply.github.com>
2022-01-31 12:18:37 -0500

    Set up parsing and logging
    Following the format of the SAS test harness.

--- src/harness/test_main.py
+++ src/harness/test_main.py
@@ -16,3 +16,31 @@
 If writing your own top level script to run the test harness,
 look here how the setup is performed.
 """
+
+import argparse
+import logging
+import sys
+
+#----------------------------------------
+# Setup the command line arguments
+parser = argparse.ArgumentParser(description='Test Main')
+# - Generic config.
+parser.add_argument('--log_level', type=str, default='info',
+                    help='Logging level: debug, info, warning or error')
+
+# Setup the logger
+logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(
+    logging.Formatter(
+        '[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d %(message)s'))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+_LOGGER_MAP = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR
+}
+
